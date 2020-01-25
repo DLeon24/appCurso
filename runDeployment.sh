81ddce1319a784686f47c047fa7d142353bc9d47
dleon <diego.a.leon24@gmail.com>
2020-01-24 19:52:38 -0500

    presentacion

--- runDeployment.sh
+++ runDeployment.sh
@@ -5,4 +5,3 @@ docker build -t $dockerImageName .
 docker run -d -p 80:80 $dockerImageName
 dockerImageId=`docker images | grep $dockerImageName | grep latest | awk -F " " '{print $3}'`
 docker tag $dockerImageId $dockerImageName:$version
-
